fbc52dff439d05562a18b35914d97d551224d594
Oliver Gosling <oliver.gosling@bigfan.agency>
2020-02-20 11:12:05 +0000

    list pagination and filtering

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # listpaginationfiltering
 
 This program paginates lists of students. Users can browse by clicking through paged navigation at the bottom of the screen, each screen displays ten students at a time.
+
+This project uses various features from javascript including:
+
+* query selector functions to select elements from the DOM
+* loops to iterate through DOM elements
+* functions to create and add new DOM elements 
+* an event listener to detect when menu items are clicked on
+* ES6 arrow functions
+
+Additional features have been added
+
+* ability to search through the students
--- js/script.js
+++ js/script.js
@@ -2,49 +2,210 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+const listItemElements = document.querySelectorAll('.student-item');
+const noOfItemsPerPage = 10;
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/**
+ * Display the students from specfied page
+ * @param {NodeList} list A list of students
+ * @param {integer} The page number to view
+ */
+const showPage = (list, page) => {
+  console.log(list);
+  const startIndex = (page * noOfItemsPerPage) - noOfItemsPerPage;
+  const endIndex = page * noOfItemsPerPage;
 
+  let i;
+  for (i = 0; i < list.length; i++){
+    if( (i >= startIndex && i < endIndex)){
+      list[i].style.display = 'block';
+    }else{
+      list[i].style.display = '';
+    }
+  }
+}
 
+/**
+ * Remove active class from pagination menu
+ */
+const clearActiveMenuItems = () => {
+  const paginationLinks = document.querySelectorAll('.pagination a');
+  paginationLinks.forEach((link) => {
+    link.classList.remove('active');
+  });
+}
 
+/**
+ * Set 'active' class on specified menu item index
+ * @param {integer} Menu index to set active
+ */
+const setActiveMenuItem = (index) => {
+  const menuItem = document.querySelectorAll('.pagination a')[--index];
+  menuItem.classList.add('active');
+}
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+/*
+ * Create menu item elements based on the number of students
+ * and the specified number of students per page. Add event
+ * listeners to detect when page items are clicked on then 
+ * call a function to display the selected page.
+ * @param {NodeList} A list of students
+ */
+const appendPageLinks = (list) => {
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+  // remove any existing pagination menu
+  const existingPaginationElement = document.querySelector('.pagination');
+  if(existingPaginationElement){
+    existingPaginationElement.remove();
+  }
 
+  // calculate number of pages only including matched items
+  const matchedItems = [...listItemElements].filter(e => {
+    return !e.classList.contains('not-matched');
+  });
 
+  const noOfPages = matchedItems.length / noOfItemsPerPage;
 
+  const newLi = (index) => {
+    const anchor = document.createElement('a');
+    const li = document.createElement('li');
+    anchor.innerText = index+1;
+    li.href = "#";
+    li.appendChild(anchor);
+    return li;
+  }
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+  // create new pagination div and append to page div
+  const paginationElement = document.createElement('div');
+  paginationElement.classList.add('pagination');
+  document.querySelector('.page').appendChild(paginationElement);
 
+  // add menu items
+  let i = 0;
+  while (i < noOfPages) {
+    paginationElement.appendChild(newLi(i));
+    i++;
+  }
 
+  // make first menu item active
+  document.querySelector('.pagination li:first-child a').classList.add('active');
 
+  // add event listener to each menu item to show selected page
+  const paginationLinks = document.querySelectorAll('.pagination a');
+  i = 0;
+  while (i < paginationLinks.length){
+    paginationLinks[i].addEventListener('click', (e) => {
+      const anchor = e.target;
+      const index = parseInt(anchor.innerText);
 
+      clearActiveMenuItems();
+      setActiveMenuItem(index);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+      if(!isNaN(index)){
+        showPage(listItemElements, index);
+      }
+    });
+    i++;
+  }
+}
+
+/*
+ * Search list for given search term. Hide irrelevant results and display only matches.
+ * @param {string} Search term
+ * @return {NodeList} The list of matching students
+ */
+const search = (searchTerm) => {
+
+    const studentItems = document.querySelectorAll('.student-item');
+
+    studentItems.forEach( (el) => {
+      const studentName = el.querySelector('h3').innerText;
+      const regexp = new RegExp(searchTerm, 'g');
+      // remove all classes
+      el.classList.remove('matched');
+      el.classList.remove('not-matched');
+      // initially hide all students
+      el.style.display = '';
+
+      if(studentName.match(regexp)){
+        el.classList.add('matched');
+      }
+      else{
+        el.classList.add('not-matched');
+      }
+    });
+
+    return document.querySelectorAll('.matched');
+}
+
+/**
+ * Create and add search bar to page
+ */
+const addSearchBar = () => {
+
+  /*
+   * Perform search
+   */
+  const performSearch = () => {
+    const searchInputText = searchInput.value;
+    const matchedItems = search(searchInputText);
+
+    // remove not found message if exists
+    const notFoundMessage = document.querySelector('.not-found-message');
+    if(notFoundMessage){
+      notFoundMessage.remove();
+    }
+
+    // if matches found then update page navigation and display first page of results
+    if(matchedItems.length){
+      appendPageLinks();
+      showPage(matchedItems, 1);
+    }
+    // else display a message stating no results have been found
+    else{
+      const notFoundParagraph = document.createElement('p');
+      notFoundParagraph.innerText = "No results found.";
+      notFoundParagraph.classList.add('not-found-message');
+      document.querySelector('.page').appendChild(notFoundParagraph);
+
+      // hide navigation since there are no results to naviagte through
+      const pagination = document.querySelector('.pagination');
+      pagination.style.display = 'none';
+    }
+  }
+
+  const searchBar = document.createElement('div');
+  searchBar.classList.add('student-search');
+
+  const searchInput = document.createElement('input');
+  searchInput.placeholder = 'Search for students...';
+
+  const searchButton = document.createElement('button');
+  searchButton.innerText = "Search";
+
+  searchBar.appendChild(searchInput);
+  searchBar.appendChild(searchButton);
+
+  const pageHeader = document.querySelector('.page-header');
+  pageHeader.appendChild(searchBar);
+
+  // on button click perform search
+  searchButton.addEventListener('click', (el) => {
+    performSearch();
+  });
+
+  // on keyup click perform search
+  searchInput.addEventListener('keyup', (el) => {
+    performSearch();
+  });
+}
+
+// add the page navigation
+appendPageLinks();
+
+// add the search bar
+addSearchBar();
+
+// show the first page
+showPage(listItemElements, 1);
